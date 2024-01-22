d7d0bbb535200ea9789930f24dda04f3e22f1617
Thuy Nguyen <phuthuynguyenn@gmail.com>
2024-01-22 14:31:10 +0700

    end project

--- controllers/products.js
+++ controllers/products.js
@@ -5,9 +5,30 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numberFilters } = req.query;
   const queryObject = {};
 
+  if (numberFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numberFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+   const options = ['price','rating']
+   filters = filters.split(",").forEach(item=>{
+    const [field,operator,value] = item.split('-')
+    if(options.includes(field)){
+      queryObject[field] = {[operator]:Number(value)}
+    }
+   })
+  }
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
@@ -33,7 +54,7 @@ const getAllProducts = async (req, res) => {
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
   result = result.skip(skip).limit(limit);
-  
+
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
